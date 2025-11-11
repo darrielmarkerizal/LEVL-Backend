75e0d5056648e4439ea5446c11dae74e565c30a1
Darriel Markerizal <darrielmarkerizal@gmail.com>
2025-11-11 23:02:31 +0700

    fix

--- Modules/Auth/database/migrations/2025_11_02_101115_create_users_table.php
+++ Modules/Auth/database/migrations/2025_11_02_101115_create_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         if (!Schema::hasTable('users')) {
-            Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
             $table->string('username', 50)->unique();
@@ -22,7 +22,7 @@ return new class extends Migration {
             $table->softDeletes();
 
             $table->index(['status', 'created_at']);
-            });
+        });
         }
     }
 
