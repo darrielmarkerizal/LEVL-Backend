33d35a4691110f828cbab236364216a8b82f0c1d
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-01-16 15:58:38 +0700

    test octane

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ Route::get("/", function () {
 });
 
 // Load test routes
+// Load test routes
 require __DIR__ . "/test-browser.php";
+
+Route::get('/dev/octane-check', [App\Http\Controllers\DevController::class, 'checkOctane']);
+
