72b3ffa3d35806c9b378d005f318b71f68d94186
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-01-18 12:57:16 +0700

    remove test

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,10 @@ Route::get("/", function () {
   return view("welcome");
 });
 
-// Load test routes
-// Load test routes
 require __DIR__ . "/test-browser.php";
 
 Route::get('/dev/octane-check', [App\Http\Controllers\DevController::class, 'checkOctane']);
 Route::get('/dev/benchmark', [App\Http\Controllers\DevController::class, 'benchmarkView']);
-
-// Benchmark API - Remove session middleware for performance
 Route::get('/dev/benchmark-api', [App\Http\Controllers\DevController::class, 'benchmarkApi'])
     ->withoutMiddleware([
         \Illuminate\Session\Middleware\StartSession::class,
