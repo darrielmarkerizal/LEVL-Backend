d8a6ce9bccc0525a6aaa1724deda05e73dff4fa1
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-04-14 20:56:21 +0700

    fix name conflict

--- Modules/Grading/routes/api.php
+++ Modules/Grading/routes/api.php
@@ -13,7 +13,7 @@ Route::middleware(['auth:api'])->prefix('v1')->group(function () {
     });
 
     Route::middleware(['role:Superadmin|Admin|Instructor'])->prefix('submissions/{submission}/grades')->group(function () {
-        Route::get('/', [GradingController::class, 'getGrade'])->name('grading.show');
+        Route::get('/', [GradingController::class, 'getGrade'])->name('grading.get');
         Route::post('/', [GradingController::class, 'manualGrade'])->name('grading.store');
         Route::put('draft', [GradingController::class, 'saveDraftGrade'])->name('grading.save-draft');
         Route::patch('/', [GradingController::class, 'overrideGrade'])->name('grading.override');
