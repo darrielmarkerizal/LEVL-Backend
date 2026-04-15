b0922e42d523ea51c06c36fef1218872e04ec1d0
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-04-15 23:15:42 +0700

    fix

--- Modules/Auth/database/migrations/2025_11_02_101115_create_users_table.php
+++ Modules/Auth/database/migrations/2025_11_02_101115_create_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        if (! Schema::hasTable('users')) {
+        
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
                 $table->string('name', 100);
@@ -24,7 +24,7 @@ return new class extends Migration
 
                 $table->index(['status', 'created_at']);
             });
-        }
+        
     }
 
     public function down(): void
