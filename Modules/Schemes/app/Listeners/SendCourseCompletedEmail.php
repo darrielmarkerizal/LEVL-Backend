44bf9db9708ef2234c75d5e8adcd33eb4686058a
darrielmarkerizal <darrielmarkerizal@gmail.com>
2026-05-03 13:30:28 +0000

    fix

--- Modules/Schemes/app/Listeners/SendCourseCompletedEmail.php
+++ Modules/Schemes/app/Listeners/SendCourseCompletedEmail.php
@@ -14,7 +14,7 @@ class SendCourseCompletedEmail implements ShouldQueue
 {
     use Queueable;
 
-    public string $queue = 'emails-transactional';
+    public $queue = 'emails-transactional';
 
     public int $tries = 3;
 
